c4927c688df50dbdad39239a704486c02dead0e7
Thao Phan <thao_phan@cs.ucsb.edu>
2021-04-23 00:20:50 -0700

    refactored code to make API endpoint computation look better. Scuttle can now get a player's match history given the player's accountID

--- commands/Lookup.js
+++ commands/Lookup.js
@@ -1,43 +1,62 @@
 const credentials = require("../credentials.json");
 const fetch = require("node-fetch")
 
-async function LookUp(region, name) {
-    let summoner = await getSummoner(region, name)
-    let rankInfo = await getRank(region, summoner.id)
-}
+let defaultURL = ''
 
-async function getSummoner(region, name) {
-    let summonerInfo = null
-    let num = null;
+async function LookUp(region, name) {
     if (region === "las") num = 2;
     else if (region != "kr" && region != "ru") num = 1;
-    const URL = `https://${region}${num == null ? "" : num
-        }.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${credentials.api_key
+    defaultURL = `https://${region}${num == null ? "" : num
+        }.api.riotgames.com/lol/`
+    const summoner = await getSummoner(defaultURL, name)
+    const rankInfo = await getRank(defaultURL, summoner.id)
+    const matches = await getMatchHistory(defaultURL, summoner.accountId)
+
+    console.log(summoner)
+    console.log(rankInfo)
+    console.log(matches)
+}
+
+async function getSummoner(defaultURL, name) {
+    const URL = defaultURL + `summoner/v4/summoners/by-name/${name}?api_key=${credentials.api_key
         }`;
     
     // response is a single JSON object
     const response = await fetch(URL)
     const json = await response.json()
-    summonerInfo = json
     
-    return summonerInfo
+    return json
 }
 
-async function getRank(region, summonerId) {
-    let summoner = null
-    let num = null;
-    if (region === "las") num = 2;
-    else if (region != "kr" && region != "ru") num = 1;
-    const URL = `https://${region}${num == null ? "" : num
-        }.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${credentials.api_key
+async function getRank(defaultURL, summonerId) {
+    const URL = defaultURL + `league/v4/entries/by-summoner/${summonerId}?api_key=${credentials.api_key
         }`;
     
     // response is an array of one JSON object
     const response = await fetch(URL)
     const json = await response.json()
-    summoner = json[0]
     
-    return summoner
+    return json[0]
+}
+
+async function getMatchHistory(defaultURL, accountId) {
+    const URL = defaultURL + `match/v4/matchlists/by-account/${accountId}?api_key=${credentials.api_key
+        }`;
+
+    // response is an array of JSON objects
+    const response = await fetch(URL)
+    const json = await response.json()
+
+    let matchHistory = []
+    for (let i = 0; i < json.matches.length; i++) {
+        tempJSON = {
+            role: json.matches[i].role,
+            lane: json.matches[i].lane
+        }
+        matchHistory.push(tempJSON)
+    }
+
+    return matchHistory
 }
 
 module.exports = { LookUp };
