f9426fcc03aad83dc791267ab7df376c23472b5e
Thao Phan <thaophan000@gmail.com>
2021-04-26 16:54:20 -0700

    Reformatted all code with Prettier, no changes made to logic

--- commands/Lookup.js
+++ commands/Lookup.js
@@ -1,117 +1,120 @@
-const credentials = require("../credentials.json");
-const fetch = require("node-fetch")
+const credentials = require('../credentials.json')
+const fetch = require('node-fetch')
 
 let defaultURL = ''
 
 async function LookUp(region, name) {
-    if (region === "las") num = 2;
-    else if (region != "kr" && region != "ru") num = 1;
-    defaultURL = `https://${region}${num == null ? "" : num
-        }.api.riotgames.com/lol/`
-    const summoner = await getSummoner(defaultURL, name)
-    if (summoner == null) return null
-    const rankInfo = await getRank(defaultURL, summoner.id)
-    const matches = await getMatchHistory(defaultURL, summoner.accountId)
+	if (region === 'las') num = 2
+	else if (region != 'kr' && region != 'ru') num = 1
+	defaultURL = `https://${region}${
+		num == null ? '' : num
+	}.api.riotgames.com/lol/`
+	const summoner = await getSummoner(defaultURL, name)
+	if (summoner == null) return null
+	const rankInfo = await getRank(defaultURL, summoner.id)
+	const matches = await getMatchHistory(defaultURL, summoner.accountId)
 
-    let matchHistory = []
-    for (let i = 0; i < matches.length; i++) {
-        matchHistory.push(await getMatchInfo(defaultURL, matches[i].gameId, name))
-    }
+	let matchHistory = []
+	for (let i = 0; i < matches.length; i++) {
+		matchHistory.push(await getMatchInfo(defaultURL, matches[i].gameId, name))
+	}
 
-    const playerInfo = {
-        summoner: summoner,
-        rankInfo: rankInfo,
-        matchHistory: matchHistory
-    }
+	const playerInfo = {
+		summoner: summoner,
+		rankInfo: rankInfo,
+		matchHistory: matchHistory,
+	}
 
-    return playerInfo
+	return playerInfo
 }
 
 async function getSummoner(defaultURL, name) {
-    const URL = defaultURL + `summoner/v4/summoners/by-name/${name}?api_key=${credentials.api_key
-        }`;
-    
-    // response is a single JSON object
-    const response = await fetch(URL)
-    const json = await response.json()
-    
-    return json
+	const URL =
+		defaultURL +
+		`summoner/v4/summoners/by-name/${name}?api_key=${credentials.api_key}`
+
+	// response is a single JSON object
+	const response = await fetch(URL)
+	const json = await response.json()
+
+	return json
 }
 
 async function getRank(defaultURL, summonerId) {
-    const URL = defaultURL + `league/v4/entries/by-summoner/${summonerId}?api_key=${credentials.api_key
-        }`;
-    
-    // response is an array of one JSON object
-    const response = await fetch(URL)
-    const json = await response.json()
-    
-    return json[0]
+	const URL =
+		defaultURL +
+		`league/v4/entries/by-summoner/${summonerId}?api_key=${credentials.api_key}`
+
+	// response is an array of one JSON object
+	const response = await fetch(URL)
+	const json = await response.json()
+
+	return json[0]
 }
 
 async function getMatchHistory(defaultURL, accountId) {
-    const URL = defaultURL + `match/v4/matchlists/by-account/${accountId}?api_key=${credentials.api_key
-        }`;
+	const URL =
+		defaultURL +
+		`match/v4/matchlists/by-account/${accountId}?api_key=${credentials.api_key}`
 
-    // response is an array of JSON objects
-    const response = await fetch(URL)
-    const json = await response.json()
-    if (!json) return null
+	// response is an array of JSON objects
+	const response = await fetch(URL)
+	const json = await response.json()
+	if (!json) return null
 
-    let matchHistory = []
-    for (let i = 0; i < json.matches.length; i++) {
-        tempJSON = {
-            gameId: json.matches[i].gameId,
-        }
-        if (json.matches[i].queue >= 400 && json.matches[i].queue <= 440)
-            matchHistory.push(tempJSON)
-        if (matchHistory.length > 19)
-            break
-    }
+	let matchHistory = []
+	for (let i = 0; i < json.matches.length; i++) {
+		tempJSON = {
+			gameId: json.matches[i].gameId,
+		}
+		if (json.matches[i].queue >= 400 && json.matches[i].queue <= 440)
+			matchHistory.push(tempJSON)
+		if (matchHistory.length > 19) break
+	}
 
-    return matchHistory
+	return matchHistory
 }
 
 async function getMatchInfo(defaultURL, matchId, name) {
-    const URL = defaultURL + `match/v4/matches/${matchId}?api_key=${credentials.api_key
-    }`;
+	const URL =
+		defaultURL + `match/v4/matches/${matchId}?api_key=${credentials.api_key}`
 
-    // response is a JSON object
-    const response = await fetch(URL)
-    const json = await response.json()
+	// response is a JSON object
+	const response = await fetch(URL)
+	const json = await response.json()
 
-    let participantId = null
-    let player = null
-    let playerStats = {
-        champion: 0,
-        win: null,
-        role: "",
-        lane: "",
-        kills: 0,
-        deaths: 0,
-        assists: 0
-    }
+	let participantId = null
+	let player = null
+	let playerStats = {
+		champion: 0,
+		win: null,
+		role: '',
+		lane: '',
+		kills: 0,
+		deaths: 0,
+		assists: 0,
+	}
 
-    // if (json.gameType != "MATCHED_GAME") return null
+	// if (json.gameType != "MATCHED_GAME") return null
 
-    for (let i = 0; i < json.participantIdentities.length; i++) {
-        const participant = json.participantIdentities[i]
-        if (participant.player.summonerName.toLowerCase() === name) {
-            participantId = participant.participantId - 1
-            break
-        }
-    }
+	for (let i = 0; i < json.participantIdentities.length; i++) {
+		const participant = json.participantIdentities[i]
+		if (participant.player.summonerName.toLowerCase() === name) {
+			participantId = participant.participantId - 1
+			break
+		}
+	}
 
-    player = json.participants[participantId]
-    playerStats.champion = player.championId
-    playerStats.win = player.stats.win
-    playerStats.role = player.timeline.role
-    playerStats.lane = player.timeline.lane
-    playerStats.kills = player.stats.kills
-    playerStats.deaths = player.stats.deaths
-    playerStats.assists = player.stats.assists
+	player = json.participants[participantId]
+	playerStats.champion = player.championId
+	playerStats.win = player.stats.win
+	playerStats.role = player.timeline.role
+	playerStats.lane = player.timeline.lane
+	playerStats.kills = player.stats.kills
+	playerStats.deaths = player.stats.deaths
+	playerStats.assists = player.stats.assists
 
-    return playerStats
+	return playerStats
 }
 
-module.exports = { LookUp };
+module.exports = { LookUp }
--- index.js
+++ index.js
@@ -1,50 +1,47 @@
-const Discord = require("discord.js")
-const credentials = require("./credentials.json")
-const LookUp = require("./commands/Lookup")
-const regions = require("./regions.json")
-const champions = require("./champions")
-const stats = require("./stats")
-const messages = require("./messages")
+const Discord = require('discord.js')
+const credentials = require('./credentials.json')
+const LookUp = require('./commands/Lookup')
+const regions = require('./regions.json')
+const champions = require('./champions')
+const stats = require('./stats')
+const messages = require('./messages')
 
 const client = new Discord.Client()
-const prefix = "-"
+const prefix = '-'
 
-client.once("ready", () => {
-    console.log("Scuttle is online!")
+client.once('ready', () => {
+	console.log('Scuttle is online!')
 })
 
-client.on("message", message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return
+client.on('message', (message) => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return
 
-    const args = message.content.slice(prefix.length).split(" ")
-    const command = args.shift().toLowerCase()
+	const args = message.content.slice(prefix.length).split(' ')
+	const command = args.shift().toLowerCase()
 
-    if (command === "l" || command === "lookup")
-    {
-        if (args.length == 0)
-        {
-            message.channel.send("-l <region> <summoner's name>")
-            return
-        }
-        const region = args.shift().toLowerCase()
-        if (!regions.includes(region))
-        {
-            message.channel.send(`${region} is not a valid region`)
-            return
-        }
+	if (command === 'l' || command === 'lookup') {
+		if (args.length == 0) {
+			message.channel.send("-l <region> <summoner's name>")
+			return
+		}
+		const region = args.shift().toLowerCase()
+		if (!regions.includes(region)) {
+			message.channel.send(`${region} is not a valid region`)
+			return
+		}
 
-        const getPlayerInfo = async () => {
-            let playerInfo = await LookUp.LookUp(region, args.join(" "))
-            if (playerInfo == null) return
-            console.log(playerInfo)
-            playerStats = stats.getPlayerStats(playerInfo.matchHistory)
-            console.log(playerStats)
-            const embed = messages.LookUpMessage(playerInfo, playerStats)
-            message.channel.send(embed)
-        }
+		const getPlayerInfo = async () => {
+			let playerInfo = await LookUp.LookUp(region, args.join(' '))
+			if (playerInfo == null) return
+			console.log(playerInfo)
+			playerStats = stats.getPlayerStats(playerInfo.matchHistory)
+			console.log(playerStats)
+			const embed = messages.LookUpMessage(playerInfo, playerStats)
+			message.channel.send(embed)
+		}
 
-        getPlayerInfo()
-    }
+		getPlayerInfo()
+	}
 })
 
 client.login(credentials.bot_token)
