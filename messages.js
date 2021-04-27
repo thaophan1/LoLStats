7530e5f127711be5837ac80a00bc963bd42160ba
Thao Phan <thaophan000@gmail.com>
2021-04-26 17:01:59 -0700

    Reformatted code with Prettier

--- messages.js
+++ messages.js
@@ -1,32 +1,32 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js')
 
 function LookUpMessage(info, stats) {
-  const summoner = info.summoner;
-  const rankInfo = info.rankInfo;
-  const topChampions = stats.champions;
-  let WR = ((rankInfo.wins / (rankInfo.losses + rankInfo.wins)) * 100).toFixed(
-    2
-  );
-  const embed = new Discord.MessageEmbed()
-    .setTitle(`${summoner.name} \tLevel: ${summoner.summonerLevel}`)
-    .setColor("RANDOM")
-    .addFields(
-      {
-        name: "Rank",
-        value: `${rankInfo.tier} ${rankInfo.rank} \n${rankInfo.leaguePoints} LP`,
-        inline: true,
-      },
-      { name: "Win Rate", value: `${WR}%`, inline: true },
-      {
-        name: "KDA",
-        value: `${stats.kills}/${stats.deaths}/${stats.assists}`,
-        inline: true,
-      },
-      { name: "Primary Role", value: `${stats.primaryRole}`, inline: true },
-      { name: "Seconday Role", value: `${stats.secondaryRole}`, inline: true }
-    );
+	const summoner = info.summoner
+	const rankInfo = info.rankInfo
+	const topChampions = stats.champions
+	let WR = ((rankInfo.wins / (rankInfo.losses + rankInfo.wins)) * 100).toFixed(
+		2
+	)
+	const embed = new Discord.MessageEmbed()
+		.setTitle(`${summoner.name} \tLevel: ${summoner.summonerLevel}`)
+		.setColor('RANDOM')
+		.addFields(
+			{
+				name: 'Rank',
+				value: `${rankInfo.tier} ${rankInfo.rank} \n${rankInfo.leaguePoints} LP`,
+				inline: true,
+			},
+			{ name: 'Win Rate', value: `${WR}%`, inline: true },
+			{
+				name: 'KDA',
+				value: `${stats.kills}/${stats.deaths}/${stats.assists}`,
+				inline: true,
+			},
+			{ name: 'Primary Role', value: `${stats.primaryRole}`, inline: true },
+			{ name: 'Seconday Role', value: `${stats.secondaryRole}`, inline: true }
+		)
 
-  return embed;
+	return embed
 }
 
-module.exports = { LookUpMessage };
+module.exports = { LookUpMessage }
